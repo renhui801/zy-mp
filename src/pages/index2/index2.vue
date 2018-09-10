88d6b8620a7c5b4e97820cab722541f5061fefdd
LiuNian <344763996@qq.com>
2018-09-10 17:55:39 +0800

    index

---
+++ src/pages/index2/index2.vue
@@ -0,0 +1,143 @@
+<template>
+
+    <div>
+    <div class="sliderDiv">
+        <div class="swiper-container">
+              <div class="swiper-wrapper">
+                  <div class="swiper-slide"><a href="javascript:void(0)"><img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/ban_1.png" /></a></div>
+                  <div class="swiper-slide"><a href="javascript:void(0)"><img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/ban_1.png" /></a></div>
+                  <div class="swiper-slide"><a href="javascript:void(0)"><img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/ban_1.png" /></a></div>
+                  <div class="swiper-slide"><a href="javascript:void(0)" ><img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/ban_1.png" /></a></div> 
+              </div>
+              <div class="swiper-pagination"></div>
+            </div>
+      </div>
+  <!--**********九宫格*************-->
+            <div class="nine">
+              <ul>
+                <li>
+                  <a href="javascript:void(0)">
+                  <img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/icon_1.png" onerror="this.src='imgs/loading.png'" />
+                  <p>商务印刷</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="javascript:void(0)">
+                  <img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/icon_2.png" onerror="this.src='imgs/loading.png'" />
+                  <p>数码印刷</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="javascript:void(0)">
+                  <img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/icon_3.png" onerror="this.src='imgs/loading.png'" />
+                  <p>广告物料</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="javascript:void(0)">
+                  <img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/icon_4.png" onerror="this.src='imgs/loading.png'" />
+                  <p>电商包装</p>
+                  </a>
+                </li>            
+                <li>
+                  <a href="javascript:void(0)">
+                  <img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/icon_5.png" onerror="this.src='imgs/loading.png'" />
+                  <p>个性定制</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="javascript:void(0)">
+                  <img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/icon_6.png" onerror="this.src='imgs/loading.png'" />
+                  <p>办公用品</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="javascript:void(0)">
+                  <img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/icon_7.png" onerror="this.src='imgs/loading.png'" />
+                  <p>纸张纸浆</p>
+                  </a>
+                </li>
+                <li>
+                  <a href="javascript:void(0)">
+                  <img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/icon_8.png" onerror="this.src='imgs/loading.png'" />
+                  <p>按需定制</p>
+                  </a>
+                </li>
+              </ul>
+            </div>
+  <!--***********推荐商品************-->
+  <div class="hots">
+    <p><b>推荐商品</b></p>
+    <ul>
+      <li><a><img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/goods_1.png" /></a></li>
+      <li><a><img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/goods_2.png" /></a></li>
+      <li><a><img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/goods_3.png" /></a></li>
+      <li><a><img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/goods_4.png" /></a></li>
+      <li><a><img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/goods_5.png" /></a></li>
+      <li><a><img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/goods_6.png" /></a></li>
+    </ul>
+  </div>
+  <!--***********推荐商家************-->
+  <div class="shops">
+    <p><b>推荐商家</b></p>
+    <ul>
+      <li><a>
+        <div class="fl"><img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/shops_1.png" /></div>
+        <div class="fr">
+          <h4>武汉市江汉区广告工作室<span><img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/ziying.png" /></span></h4>
+          <p class="add">少喝水少话啥时候水水水水还是说啊哈哈哈哈哈</p>
+          <p class="num"><em>已售：345单</em><span>0.98KM</span></p>
+        </div>
+      </a></li>
+      <li><a>
+        <div class="fl"><img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/shops_1.png" /></div>
+        <div class="fr">
+          <h4>武汉市江汉区广告工作室<span><img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/ziying.png" /></span></h4>
+          <p class="add">少喝水少话啥时候水水水水还是说啊哈哈哈哈哈</p>
+          <p class="num"><em>已售：345单</em><span>0.98KM</span></p>
+        </div>
+      </a></li>
+      <li><a>
+        <div class="fl"><img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/shops_1.png" /></div>
+        <div class="fr">
+          <h4>武汉市江汉区广告工作室<span><img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/ziying.png" /></span></h4>
+          <p class="add">少喝水少话啥时候水水水水还是说啊哈哈哈哈哈</p>
+          <p class="num"><em>已售：345单</em><span>0.98KM</span></p>
+        </div>
+      </a></li>
+      
+    </ul>
+  </div>
+  <div class="car">
+    <a href="javascript:void(0)"><img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/car.png" class="car" /></a>
+  </div>
+  <div class="kefu">
+    <img src="https://ziyunoss.oss-cn-hangzhou.aliyuncs.com/upload/wap/littileminiprogram/index/kefu.png" class="kefu" />
+  </div>
+    </div>
+</template>
+
+
+
+<script>
+
+export default {
+data () {
+    return {
+      test: 1,
+      backgroundColor:true
+    }
+  },
+  methods:{
+    test1:function(e){
+     this.test=e.target.value;
+    },
+    changecolor:function () {
+      this.backgroundColor=!this.backgroundColor;
+    }
+  },
+  computed:{
+  }
+
+}
+</script>
---
+++ src/pages/index2/main.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import Test from './index2'
+const app = new Vue(Test)
+app.$mount()
+import  "./index.css";
+// import swiper from './swiper.min.js';
